2efc2032be4764c9fb1e757488ef9d8b62cb5c10
Abdulrahman Abbas <54155369+abbas-abdelrhman@users.noreply.github.com>
2023-05-17 09:11:27 +0200

    Update main.py

--- individuals-spiders/main.py
+++ individuals-spiders/main.py
@@ -9,8 +9,8 @@ import json
 
 
 def jira_tickets():
-    auth = HTTPBasicAuth("abdelrahmanabbas@rentola.com",
-                         "ATATT3xFfGF0DfHikWVALUl1pUOseFl9GHHFHB99T5mZYvDiBErDESLVdechg4giVAT09ZBN9lVtsC2tUQb8AKns5RhYgKy_nJnjSUaVx01HmWdtjHYFyjANP75k3Yj5-s_OQTedNtSCajRhBwUp2VlbzPB9DbyRRv2XEjzWXHQ1-Fh3JzZo3Jk=D7F14C08")
+    auth = HTTPBasicAuth("email",
+                         "auth-code")
 
     url = """https://rentola-dev.atlassian.net/rest/api/3/search?jql=project%20%3D%20"SFR"%20AND%20status%20%3D%20"Waiting%204%20Automatic%20Submission"%20AND%20updated%20<%3D%20-1w%20ORDER%20BY%20assignee%20ASC%2C%20created%20DESC%2C%20issuekey%20ASC%2C%20summary%20ASC&referrer=agility&maxResults=100"""
 
@@ -27,7 +27,7 @@ def jira_tickets():
     )
 
     for issue in json.loads(response.text).get('issues'):
-        tickets[f"https://rentola-dev.atlassian.net/browse/{issue.get('key')}"] = issue.get('fields').get('summary')
+        tickets[f"https://rentola.atlassian.net/browse/{issue.get('key')}"] = issue.get('fields').get('summary')
 
     return tickets
 
